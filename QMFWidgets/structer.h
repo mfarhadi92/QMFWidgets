6c263c1c16d21daf8fb4b56a06039a599aec35d2
mehdi farhadi 92 <35996345+mfarhadi92@users.noreply.github.com>
2018-11-04 11:04:37 +0330

    Update structer.h
    add default status color

--- QMFWidgets/structer.h
+++ QMFWidgets/structer.h
@@ -8,6 +8,8 @@ namespace QtMFW {
 static const QStringList alertIcon = QStringList()<<":/image/res/images/status/success.png"<<":/image/res/images/status/success.png"<<
                                                    ":/image/res/images/status/warning.png"<<":/image/res/images/status/error.png"<<"";
 enum Status{processing,successed,warning,failed,empty};
+static const QStringList StatusColor = QStringList() << "#2196f3" << "#4cb74c" << "#f9a937" << "#ff4c4c" << "#4c4c4c";
+  
 struct AlertInfo {
     QString text;
     Status status = successed;
